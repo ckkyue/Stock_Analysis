652f76c8bda53e512d10a68ba5c44688e926ea97
Kelvin Yue <ckkyue@connect.ust.hk>
2024-09-24 01:55:47 +0800

    In progress

--- Program/test_stock.py
+++ Program/test_stock.py
@@ -15,16 +15,16 @@ current_date = start.strftime("%Y-%m-%d")
 # Choose the stocks
 stocks = ["ACIW", "NRG", "CRS"]
 
-# Iterate over stocks
-for stock in stocks:
-    df = get_df(stock, current_date)
-    plot_close(stock, df, save=True)
-    plot_MFI_RSI(stock, df, save=True)
-    plot_stocks(["^GSPC", "^GSPC", stock], current_date, save=True)
+# # Iterate over stocks
+# for stock in stocks:
+#     df = get_df(stock, current_date)
+#     plot_close(stock, df, save=True)
+#     plot_MFI_RSI(stock, df, save=True)
+#     plot_stocks(["^GSPC", "^GSPC", stock], current_date, save=True)
 
 # Get the stop loss and target price of a stock
 stock = "ACIW"
-stoploss, stoploss_pct, target, target_pct = stoploss_target(stock, 50.33, current_date)
+stoploss, stoploss_pct, target, target_pct = stoploss_target(stock, 49.623, current_date)
 print(f"Stoploss: {stoploss}, {stoploss_pct} (%).")
 print(f"Target price: {target}, {target_pct} (%).")
 
