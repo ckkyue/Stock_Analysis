46397b6810abea1350ee5d550e57a5fd5481f7d1
Kelvin Yue <ckkyue@connect.ust.hk>
2024-10-06 16:17:36 +0800

    In progress

--- Program/test_stock.py
+++ Program/test_stock.py
@@ -15,8 +15,8 @@ start = dt.datetime.now()
 # Initial setup
 current_date = start.strftime("%Y-%m-%d")
 
-# Choose the stocks
-stocks = ["ACIW", "PI", "PLTR"]
+# # Choose the stocks
+# stocks = ["ACIW", "PI", "PLTR"]
 
 # # Iterate over stocks
 # for stock in stocks:
@@ -25,14 +25,14 @@ stocks = ["ACIW", "PI", "PLTR"]
 #     plot_MFI_RSI(stock, df, save=True)
 #     plot_stocks(["^GSPC", "^GSPC", stock], current_date, save=True)
 
-# Get the stop loss and target price of a stock
-stock = "PLTR"
-df = get_df(stock, current_date)
-current_close = df["Close"].iloc[-1]
-stoploss, stoploss_pct, target, target_pct = stoploss_target(stock, 38.5, current_date)
-print(f"Current close: {round(current_close, 2)}.")
-print(f"Stoploss: {stoploss}, {stoploss_pct} (%).")
-print(f"Target price: {target}, {target_pct} (%).")
+# # Get the stop loss and target price of a stock
+# stock = "PLTR"
+# df = get_df(stock, current_date)
+# current_close = df["Close"].iloc[-1]
+# stoploss, stoploss_pct, target, target_pct = stoploss_target(stock, 38.5, current_date)
+# print(f"Current close: {round(current_close, 2)}.")
+# print(f"Stoploss: {stoploss}, {stoploss_pct} (%).")
+# print(f"Target price: {target}, {target_pct} (%).")
 
 # # Choose the stock
 # stock = "^HSI"
@@ -93,6 +93,82 @@ print(f"Target price: {target}, {target_pct} (%).")
 
 #     # Show the plot
 #     plt.show()
+
+# Test the correlation between 京東 and HSI
+jd_df = get_df("9618.HK", current_date)
+hsi_df = get_df("^HSI", current_date)
+
+# Calculate percentage change for both
+show = 120
+hsi_df["HSI Percent Change"] = hsi_df["Close"].pct_change()
+jd_df["JD Percent Change"] = jd_df["Close"].pct_change()
+
+# Merge dataframes based on index
+merged_df = pd.merge(hsi_df, jd_df["JD Percent Change"], left_index=True, right_index=True, how="inner")
+merged_df = merged_df.dropna()
+
+# Select the last 'show' periods
+merged_df_res = merged_df[- show:]
+
+# Calculate the correlation factor
+hsi_pct = merged_df_res["HSI Percent Change"].to_numpy()
+jd_pct = merged_df_res["JD Percent Change"].to_numpy()
+factor_corr = np.corrcoef(hsi_pct, jd_pct)[0, 1]
+print(f"The correlation factor between HSI and 9618.HK is {factor_corr}.")
+
+# Iterate over all factors
+factors = np.arange(0, 3.01, 0.01)
+diff_sums = np.zeros(len(factors))
+for i, factor in enumerate(factors):
+    diff_sums[i] = np.sum((jd_pct - factor * hsi_pct)**2)
+
+# Find the factor of the minimum difference sum
+min_index = np.argmin(diff_sums)
+min_factor = factors[min_index]
+min_diff_sums = diff_sums[min_index]
+print(f"The HSI best approximates 9618.HK at a factor of {min_factor}, with a minimum difference of {min_diff_sums}.")
+
+# Create a figure
+plt.figure(figsize=(10, 6))
+
+# Create a leveraged hsi_df
+hsi_df["Leveraged Close"] = (hsi_df["Close"].iloc[0] * (1 + min_factor * hsi_df["HSI Percent Change"]).cumprod())
+
+# Select the last 'show' periods for plotting
+hsi_df = hsi_df[- show:].dropna()
+jd_df = jd_df[- show:].dropna()
+
+# Get the first closing price of the first stock
+close_first0 = hsi_df["Leveraged Close"].iloc[0]
+
+# Plot the closing price history of the first stock
+plt.plot(100 / close_first0 * hsi_df["Leveraged Close"], label=f"{min_factor:.2f}x HSI (scaled)")
+
+# Plot the closing price history
+close_first = jd_df["Close"].iloc[0]
+plt.plot(100 / close_first * jd_df["Close"], label="9618.HK (scaled)")
+
+# Set the x limit
+plt.xlim(hsi_df.index[0], hsi_df.index[-1])
+
+# Set the labels
+plt.xlabel("Date")
+plt.ylabel("Price")
+
+# Set the legend
+plt.legend()
+
+# Set the title
+plt.title("Closing price history for stocks")
+
+# Adjust the spacing
+plt.tight_layout()
+
+# Save the plot
+plt.savefig(f"Result/Figure/HSIJD{show}.png", dpi=300)
+
+# Show the plot
+plt.show()
 
 # Print the end time and total runtime
 end = dt.datetime.now()
